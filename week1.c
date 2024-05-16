578ada2360ee9a14573dc8178c800a8ec79768ba
1BM22CS204 <153579751+PranavY204@users.noreply.github.com>
2024-05-16 10:13:36 +0530

    Create week1.c

---
+++ week1.c
@@ -0,0 +1,10 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int k = 1;
+    for(int i = 1; i < numsSize; i++){
+        if (nums[i] != nums[k-1]){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;   
+}
