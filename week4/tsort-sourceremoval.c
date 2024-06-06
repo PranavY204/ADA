a79e60d28555b947a3c28116c851cfc92f7ca390
1BM22CS204 <153579751+PranavY204@users.noreply.github.com>
2024-06-06 09:51:32 +0530

    Update tsort-sourceremoval.c

--- week4/tsort-sourceremoval.c
+++ week4/tsort-sourceremoval.c
@@ -1,70 +1,50 @@
-void tsort(int mat[size][size], int n);
+#include <stdio.h>
 
-void tsortdfs(int mat[size][size], int n){
-    bool visited[n];
-    int stack[n], top=-1, poporder[n], pos=-1;
-    for(int i = 0; i < n; i++) visited[i] = false;
-    stack[++top] = 0;
-    visited[0] = true;
-    while(top != -1){
-        bool new_node = false;
-        for(int i = 0; i < n; i++){
-            if(mat[stack[top]][i] == 1 && !visited[i]){
-                new_node = true;
-                stack[++top] = i;
-                visited[i] = true;
-                printf("%d\n", i);
+#define MAX_VERTICES 100
 
-                break;
-            }
-        }
-        if (!new_node) poporder[++pos] = stack[top--];
-    }
-    // while(poporder != -1) printf("%d ", poporder[pos--]);
-}
+int indegree[MAX_VERTICES];
+int graph[MAX_VERTICES][MAX_VERTICES];
 
-int main(){
-    int n;
-    printf("Enter no. of vertices: ");
-    scanf("%d", &n);
-    int mat[size][size];
-    printf("Enter adjacency matrix:\n");
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            scanf("%d", &mat[i][j]);
-        }
-    }
-    tsortdfs(mat, n);
-}
+void topologicalSort(int V) {
+    int queue[MAX_VERTICES], front = 0, rear = -1;
 
-void tsort(int mat[size][size], int n){
-    int indegree[n];
-    for(int i = 0; i < n; i++){
-        indegree[i] =0;
-        for(int j = 0; j < n; j++) indegree[i] += mat[j][i];
-    }
-    // for(int i = 0; i < n; i++) printf("Indegree of %d: %d\n", i, indegree[i]);
-    int queue[n], sequence[n], front=0, rear=-1, counter=-1;
-    for(int i = 0; i < n; i++){
-        if(indegree[i] == 0){
+    for (int i = 0; i < V; i++)
+        if (indegree[i] == 0)
             queue[++rear] = i;
-        }
-    }
-    // for(int i = 0; i < n; i++) printf("Queue[%d]: %d\n", i, queue[i]);
-    printf("Topological Order: ");
-    while(front<=rear){
-        int u = queue[front++];
-        printf("%d ", u+1);
-        sequence[++counter] = u;
-        // for(int i = 0; i < n; i++) printf("%d ", sequence[i]);
-        for(int i = 0; i < n; i++){
-            if(mat[u][i] == 1){
+
+    while (front <= rear) {
+        int vertex = queue[front++];
+
+        printf("%d ", vertex);
+
+        for (int i = 0; i < V; i++)
+            if (graph[vertex][i] == 1) {
                 indegree[i]--;
                 if (indegree[i] == 0)
                     queue[++rear] = i;
-                // for(int i = 0; i < n; i++) printf("Indegree of %d: %d\n", i, indegree[i]);
             }
-
-        }
     }
+}
+
+int main() {
+    int V, E;
+
+    printf("Enter the number of vertices: ");
+    scanf("%d", &V);
+
+    printf("Enter the adjacency matrix:\n");
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            scanf("%d", &graph[i][j]);
+
+    for (int i = 0; i < V; i++) {
+        indegree[i] = 0;
+        for (int j = 0; j < V; j++)
+            indegree[i] += graph[j][i];
+    }
+
+    printf("Topological Sort: ");
+    topologicalSort(V);
+
+    return 0;
 }
